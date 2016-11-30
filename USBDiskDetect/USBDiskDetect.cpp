494559249b3a8709f1c130cdfbb096203fbf2d35
isdqwq <ic520hl@gmail.com>
2016-11-30 17:08:40 +0800

    Use GetLogicalDrives get all the drive label of the system
    Signed-off-by: isdqwq <ic520hl@gmail.com>

--- USBDiskDetect/USBDiskDetect.cpp
+++ USBDiskDetect/USBDiskDetect.cpp
@@ -2,10 +2,25 @@
 //
 
 #include "stdafx.h"
-
+#include <Windows.h>
+#include <WinIoCtl.h>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	DWORD dwLogicDiskFlag = GetLogicalDrives();
+	//disk loop form lable C,and ignore lable 'A' 'B'
+	dwLogicDiskFlag = dwLogicDiskFlag >> 2;
+	for (char diskLoop = 'C'; diskLoop <= 'Z'; diskLoop++)
+	{
+		printf("found: ");
+		if ((dwLogicDiskFlag >> (diskLoop - 'C')) & 1)
+		{
+			printf("%c:\\\r\n", diskLoop);
+			//Reduce the number of cycles
+			if (1 == (dwLogicDiskFlag >> (diskLoop - 'C')))
+				break;
+		}
+	}
 	return 0;
 }
 
